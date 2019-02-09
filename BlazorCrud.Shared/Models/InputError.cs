4c64f437e778c770a31d8b280348c6506c14d430
Uğuralp ÖNBAŞLI <uguralponbasli@hotmail.com>
2019-02-09 01:24:21 -0500

    Add the Project
    The project added to GitHub.

---
+++ BlazorCrud.Shared/Models/InputError.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorCrud.Shared.Models
+{
+    public class InputError
+    {
+        private string fullnameError;
+        private string emailError;
+        private string addressError;
+        private string cityError;
+        private string phoneNumberError;
+        private string vnameError;
+        private string vmodelError;
+        private string vyearError;
+
+        public string FullnameError { get => fullnameError; set => fullnameError = value; }
+        public string EmailError { get => emailError; set => emailError = value; }
+        public string AddressError { get => addressError; set => addressError = value; }
+        public string CityError { get => cityError; set => cityError = value; }
+        public string PhoneNumberError { get => phoneNumberError; set => phoneNumberError = value; }
+        public string VnameError { get => vnameError; set => vnameError = value; }
+        public string VmodelError { get => vmodelError; set => vmodelError = value; }
+        public string VyearError { get => vyearError; set => vyearError = value; }
+    }
+}
---
+++ BlazorCrud.Shared/Models/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace BlazorCrud.Shared.Models
+{
+    public class User
+    {
+        public int userID { get; set; }
+        [Required]
+        public string Fullname { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Address { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public string PhoneNumber { get; set; }
+        [Required]
+        public string Vname { get; set; }
+        [Required]
+        public string Vmodel { get; set; }
+        [Required]
+        public string Vyear { get; set; }
+    }
+}
